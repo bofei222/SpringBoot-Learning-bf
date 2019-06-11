f37a01e38363d50fa4324107bd0caae5566d7313
bofei <bofei222@163.com>
2019-06-11 20:05:21 +0800

    maven

--- activiti-demo6-springboot/src/main/java/com/yawn/controller/LoginController.java
+++ activiti-demo6-springboot/src/main/java/com/yawn/controller/LoginController.java
@@ -24,10 +24,6 @@ public class LoginController {
         return "login";
     }
 
-    @GetMapping("/b")
-    public String b() {
-        return "user";
-    }
 
     @PostMapping("/login")
     @ResponseBody
