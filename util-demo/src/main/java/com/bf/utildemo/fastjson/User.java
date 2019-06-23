79fd02f08eba353b5646e2cb6dc4a4b070e7f38c
bofei <bofei222@163.com>
2019-06-23 23:27:13 +0800

    fastjson

---
+++ util-demo/src/main/java/com/bf/utildemo/fastjson/User.java
@@ -0,0 +1,24 @@
+package com.bf.utildemo.fastjson;
+
+public class User {
+
+    private String nameInfo;
+
+    private String ageInfo;
+
+    public String getNameInfo() {
+        return nameInfo;
+    }
+
+    public void setNameInfo(String nameInfo) {
+        this.nameInfo = nameInfo;
+    }
+
+    public String getAgeInfo() {
+        return ageInfo;
+    }
+
+    public void setAgeInfo(String ageInfo) {
+        this.ageInfo = ageInfo;
+    }
+}
