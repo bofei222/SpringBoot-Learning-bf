1939480a20e5285e789c1d29d00361363977072d
bofei <bofei222@163.com>
2019-06-26 20:24:47 +0800

    c

--- util-demo/src/main/java/com/bf/utildemo/fastjson/Test.java
+++ util-demo/src/main/java/com/bf/utildemo/fastjson/Test.java
@@ -3,8 +3,13 @@ package com.bf.utildemo.fastjson;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.PropertyNamingStrategy;
+import com.alibaba.fastjson.parser.ParserConfig;
+import com.alibaba.fastjson.serializer.NameFilter;
+import com.alibaba.fastjson.serializer.PascalNameFilter;
 import com.alibaba.fastjson.serializer.SerializeConfig;
 import org.junit.Assert;
+
+import java.util.regex.Matcher;
 
 public class Test {
     @org.junit.Test
@@ -13,16 +18,53 @@ public class Test {
         User user = new User();
         user.setNameInfo("coder");
         user.setAgeInfo("28");
+        user.setSex_info("女");
+
+
+        // 默认驼峰风格 （错了， 默认和属性名一致）
+        System.out.println(JSON.toJSONString(user));
+
+        // 序列化配置对象
         SerializeConfig config = new SerializeConfig();
-        config.propertyNamingStrategy = PropertyNamingStrategy.SnakeCase;
+        config.propertyNamingStrategy = PropertyNamingStrategy.CamelCase;
+
+        // 序列化对象
+        // JSONField配置 大于 全局配置
         String json = JSON.toJSONString(user, config);
         System.out.println(json);
-        Assert.assertEquals("{\"age_info\":\"28\",\"name_info\":\"coder\"}", json);
+
+//        Assert.assertEquals("{\"age_info\":\"28\",\"name_info\":\"coder\"}", json);
 
 
-        JSONObject jsonObject = JSON.parseObject(json);
-        System.out.println(jsonObject);
+        // 反序列化配置对象  反序列化配置 有啥用
+        ParserConfig parserConfig = new ParserConfig();
+        parserConfig.propertyNamingStrategy = PropertyNamingStrategy.KebabCase;  // 好像没用啊
 
+
+        User user2 = JSON.parseObject(json, User.class, parserConfig);
+
+
+        String s = JSONObject.toJSONString(user2);
+        System.out.println(s);
+
+        JSONObject jo = new JSONObject();
+        jo.put("customer_name", "bf");
+
+//        String str = JSON.toJSONString(jo, new NameFilter() {
+//
+//            @Override
+//            public String process(Object object, String name, Object value) {
+//                Matcher matcher = humpPattern.matcher(name);
+//                StringBuffer sb = new StringBuffer();
+//                while (matcher.find()) {
+//                    matcher.appendReplacement(sb, "_" + matcher.group(0).toLowerCase());
+//                }
+//                matcher.appendTail(sb);
+//                return sb.toString();
+//            }
+//
+//        });
+//        System.out.println(jsonStr);
 
     }
 }
--- util-demo/src/main/java/com/bf/utildemo/fastjson/User.java
+++ util-demo/src/main/java/com/bf/utildemo/fastjson/User.java
@@ -1,10 +1,23 @@
 package com.bf.utildemo.fastjson;
 
-public class User {
+import com.alibaba.fastjson.annotation.JSONField;
 
+public class User {
+    @JSONField(name="name_Info")
     private String nameInfo;
 
     private String ageInfo;
+
+//    @JSONField(name = "sexInfo")
+    private String sex_info;
+
+    public String getSex_info() {
+        return sex_info;
+    }
+
+    public void setSex_info(String sex_info) {
+        this.sex_info = sex_info;
+    }
 
     public String getNameInfo() {
         return nameInfo;
